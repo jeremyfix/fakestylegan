2e088ea599d6289bc423b6c18519dfdb74970c63
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-02-04 18:17:37 +0100

    code to refactor using the encoder4editing encoder

--- fakestylegan/projector.py
+++ fakestylegan/projector.py
@@ -12,22 +12,34 @@ using a pretrained network
 """
 
 # Standard imports
-import urllib.request
+import logging
+
+# import urllib.request
 from pathlib import Path
 import pickle
 
 # External imports
 import torch
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 
 class Projector:
-    def __init__(self, network="stylegan3-t-ffhq-1024x1024.pkl"):
+    def __init__(self, encoder="e4e_ffhq_encode.pt"):
+        # Take it from https://github.com/omertov/encoder4editing/blob/main/scripts/inference.py
+        # It contains the decoder as well
         # network="stylegan3-r-ffhqu-256x256.pkl"):
-        p = Path(network)
+        p = Path(decoder)
         if not p.exists():
+            logging.error(
+                f"I did not find the network file {network}. You may want to download it from https://catalog.ngc.nvidia.com/orgs/nvidia/teams/research/models/stylegan3/files"
+            )
+            logging.error(
+                "For example, with : curl -LO 'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-t-ffhq-1024x1024.pkl'"
+            )
             raise NotImplementedError
+
             # TODO
             # The following never completes while the wget/curl
             # commands NVlabs provide do actually download the network
@@ -46,10 +58,20 @@ class Projector:
         img = G(z, c)  # NCHW, float32, dynamic range [-1, +1], no truncation
         img = img.squeeze().permute(1, 2, 0)
         img = img.numpy()
+        img = (1.0 + img) / 2.0
+        img = img.clip(0.0, 1.0)
         plt.figure()
-        plt.imshow((1.0 + img) / 2.0)
+        plt.imshow(img)
         plt.show()
+
+    def backproject(self, img):
+        if isinstance(img, str) or isinstance(img, Path):
+            # Consider img is a path
+            pass
+
+        if not isinstance(img, np.array):
+            pass
 
 
 if __name__ == "__main__":
-    p = Projector()
+    p = Projector(network="stylegan3-t-ffhqu-256x256.pkl")
